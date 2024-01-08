6ad0b3940c867181ff86bdb104dddc947985fcc8
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2024-01-08 08:49:41 -0500

    Update gamingItems.java

--- src/pain/gamingItems.java
+++ src/pain/gamingItems.java
@@ -20,14 +20,58 @@ public class gamingItems{
 //    public static final Seq<Item> Begone = new Seq<>();
     
     public static void load(){
-        wroughtiron = new Item("Wrought Iron", Color.valueOf("00f0ec")){{
+        wroughtiron = new Item("wrought-iron", Color.valueOf("00f0ec")){{
             localizedName = "Wrought Iron";
-               description = "sahbdja";
-                charge = 9.5f;
-                radioactivity = 0f;
-                explosiveness = 1f;
-                flammability = 0.2f;
+               description = "Early iron in the process, used in making some structures and better resources";
                 cost = 0.1f;
+                //hardness = 15;
+       }};
+        hematite = new Item("hematite", Color.valueOf("00f0ec")){{
+            localizedName = "Hematite";
+               description = "Iron ore";
+                cost = 0.1f;
+                //hardness = 15;
+       }};
+        castiron = new Item("cast-iron", Color.valueOf("00f0ec")){{
+            localizedName = "Cast Iron";
+               description = "High carbon iron. More useful than wrought iron.";
+                cost = 0.3f;
+                //hardness = 15;
+       }};
+        pigiron = new Item("pig-iron", Color.valueOf("00f0ec")){{
+            localizedName = "Pig Iron";
+               description = "Garbage iron, used only to produce better types of iron.";
+                cost = 0.3f;
+                //hardness = 15;
+       }};
+        steel = new Item("steel", Color.valueOf("00f0ec")){{
+            localizedName = "Steel";
+               description = "Strong but expensive structural material, useful in many things";
+                cost = 0.3f;
+                //hardness = 15;
+       }};
+        hotwroughtiron = new Item("hot-wrought-iron", Color.valueOf("00f0ec")){{
+            localizedName = "Hot Wrought Iron";
+               description = "Heated Wrought Iron.";
+                cost = 0.1f;
+                //hardness = 15;
+       }};
+        hotcastiron = new Item("hot-cast-iron", Color.valueOf("00f0ec")){{
+            localizedName = "Hot Cast Iron";
+               description = "Hot Cast Iron";
+                cost = 0.3f;
+                //hardness = 15;
+       }};
+        pigiron = new Item("pig-iron", Color.valueOf("00f0ec")){{
+            localizedName = "Pig Iron";
+               description = "Garbage iron, used only to produce better types of iron.";
+                cost = 0.3f;
+                //hardness = 15;
+       }};
+        steel = new Item("steel", Color.valueOf("00f0ec")){{
+            localizedName = "Steel";
+               description = "Strong but expensive structural material, useful in many things";
+                cost = 0.3f;
                 //hardness = 15;
        }};
      //           EarthItems.addAll(
